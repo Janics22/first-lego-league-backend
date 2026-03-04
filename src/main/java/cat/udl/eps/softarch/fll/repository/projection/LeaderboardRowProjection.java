948f5531f70e829edc417c30f824f4779c879950
polMarsol <marsoltorraspol@gmail.com>
2026-03-04 23:15:50 +0100

    refactor(leaderboard): simplify query formatting in MatchResultRepository and add spacing in LeaderboardRowProjection

--- src/main/java/cat/udl/eps/softarch/fll/repository/projection/LeaderboardRowProjection.java
+++ src/main/java/cat/udl/eps/softarch/fll/repository/projection/LeaderboardRowProjection.java
@@ -2,7 +2,10 @@ package cat.udl.eps.softarch.fll.repository.projection;
 
 public interface LeaderboardRowProjection {
 	String getTeamId();
+
 	String getTeamName();
+
 	Long getTotalScore();
+
 	Long getMatchesPlayed();
 }

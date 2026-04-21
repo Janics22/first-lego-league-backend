7f933822dec60e0d5445c7a24e27ee283416e930
Gerard Moliner <79200478+Mooliner@users.noreply.github.com>
2026-04-21 17:14:41 +0200

    Feat: add BDD scenario and enforce 10-member team capacity

--- src/main/java/cat/udl/eps/softarch/fll/repository/team/TeamMemberRepository.java
+++ src/main/java/cat/udl/eps/softarch/fll/repository/team/TeamMemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import cat.udl.eps.softarch.fll.domain.TeamMember;
+import cat.udl.eps.softarch.fll.domain.Team;
 
 @RepositoryRestResource(path = "teamMembers")
 public interface TeamMemberRepository extends CrudRepository<TeamMember, Long>, PagingAndSortingRepository<TeamMember, Long> {
@@ -13,5 +14,7 @@ public interface TeamMemberRepository extends CrudRepository<TeamMember, Long>, 
 	List<TeamMember> findByName(@Param("name") String name);
 	
 	List<TeamMember> findByRole(@Param("role") String role);
+
+	long countByTeam(Team team);
 }
 
